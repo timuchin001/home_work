f19bee9076ff93803f893c289531d3a6941e2398
temirlan <temirlanbekmamatov@icloud.com>
2025-11-19 12:08:09 +0600

    Refactor burger menu click handling logic

--- maket_3/js/main.js
+++ maket_3/js/main.js
@@ -1,20 +1,24 @@
 (function (){
 
-   document.addEventListener('click',burgerInit)
+   document.addEventListener('click', burgerInit);
 
-   function burgerInit (e) {
+   function burgerInit(e) {
       const burgerIcon = e.target.closest('.burger__icon');
-      const burgerNavLink = e.target.closest('.nav__list');
+      const navLink = e.target.closest('.nav__link');
 
-      if (!burgerIcon && !burgerNavLink) return
-      if (document.documentElement.clientWidth > 900) return
+      if (!burgerIcon && !navLink) return;
 
-      if (!document.body.classList.contains('body--opened-menu')) {
-      document.body.classList.add('body--opened-menu')
-   }else {
-      document.body.classList.remove('body--opened-menu')
+      if (document.documentElement.clientWidth > 900) return;
+
+      if (burgerIcon) {
+         e.preventDefault();
+         document.body.classList.toggle('body--opened-menu');
+         return;
+      }
+
+      if (navLink) {
+         document.body.classList.remove('body--opened-menu');
+      }
    }
-   
-   }
 
-})()
+})();
